e419ef459821309007f07eb3856dbf1a7bfdec83
Bruce762 <82109931+Bruce762@users.noreply.github.com>
2022-03-10 01:13:53 +0800

    Update auto.py

--- auto.py
+++ auto.py
@@ -3,4 +3,4 @@ import cv2
 
 img = pyautogui.screenshot(region=[0,0,100,100]) # x,y,w,h
 img.save('screenshot.png')
-#img = cv2.cvtColor(np.asarray(img),cv2.COLOR_RGB2BGR)
+#img = cv2.cvtColor(np.asarray(img),cv2.COLOR_RGB2BGR)1
